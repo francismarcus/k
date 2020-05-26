cef8083a446301ea64b58a2c4e4f5551661722f3
Marcus Francis <marcusmagnusson@outlook.com>
2020-05-26 22:47:52 +0200

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -2,9 +2,13 @@ import express, { Request, Response, NextFunction } from 'express';
 import { json } from 'body-parser';
 import cors from 'cors';
 
+import { Router } from './routes';
+
 const app = express();
 app.use(json());
 app.use(cors());
+
+app.use('/api/v0', Router);
 
 app.get('/', (req: Request, res: Response, next: NextFunction) => {
 	res.send('Boilerplate up and running');
